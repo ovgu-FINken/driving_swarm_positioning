41bd6d64bd190cfa308d96e9cc08369d4d34984c
= <turtlebot1@ovgu.de>
2019-11-15 13:02:46 +0000

    parse paparazzi messages and print out ranges

--- src/range_pub.py
+++ src/range_pub.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-import rospy
-from driving_swarm_positioning.msg import Range
+#import rospy
+#from driving_swarm_positioning.msg import Range
 import serial
-import pprz.py
+import pprz
 
 module = serial.Serial("/dev/ttyS0", 115200)
+parser = pprz.PprzParser(callback=pprz.parse_range)
+
 
 while True:
-   print(f"Line: {module.readln()}")
+  parser.data_in(module.read())
+
+
+
+
 
 
