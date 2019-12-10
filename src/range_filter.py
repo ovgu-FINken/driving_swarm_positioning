063dced7bac704448dd25cdfd5279d80083fe37c
jonathan <jonathan.beckhaus@live.de>
2019-12-10 14:10:53 +0100

    implement range_filter

---
+++ src/range_filter.py
@@ -0,0 +1,71 @@
+import rospy
+from driving_swarm_positioning.msg import Range
+import numpy as np
+
+class RingBuffer:
+    """ class that implements a not-yet-full buffer """
+    def _ _init_ _(self,size_max):
+        self.max = size_max
+        self.data = []
+
+    class _ _Full:
+        """ class that implements a full buffer """
+        def append(self, x):
+            """ Append an element overwriting the oldest one. """
+            self.data[self.cur] = x
+            self.cur = (self.cur+1) % self.max
+        def get(self):
+            """ return list of elements in correct order """
+            return self.data[self.cur:]+self.data[:self.cur]
+
+    def append(self,x):
+        """append an element at the end of the buffer"""
+        self.data.append(x)
+        if len(self.data) == self.max:
+            self.cur = 0
+            # Permanently change self's class from non-full to full
+            self._ _class_ _ = self._ _Full
+
+    def get(self):
+        """ Return a list of elements from the oldest to the newest. """
+        return self.data
+
+class RangeBufferFilterDings:
+    data = [] #[[start,ziel,buffer],[start,ziel,buffer],[start,ziel,buffer],...]
+    window = 10
+    def __init__(self, window=10):
+        self.data = []
+        self.window = window
+
+    def addRange(self, msg):
+        src = msg.src
+        target = msg.dest
+        range = msg.range
+        for distancearray in self.data:
+            if(distancearray[0] == src and rangeFrame[1] == target):
+                distancearray[2].append(range)
+                return distancearray
+        g = RingBuffer(self.window)
+        g.append(range)
+        self.data.append([src, target, g])
+        return self.data[len(self.data)-1]
+
+
+
+def filterfunction(DataList):
+    g = np.array(DataList)
+    return np.median(g);
+
+def callback(msg, rangefilter):
+    lastValues = rangefilter.addRange(msg);
+    newRange = filterfunction(lastValues)
+    newMsg = msg
+    newMsg.range = newRange
+    pub.publish(newMsg)
+
+
+if __name__ == '__main__':
+    rangefilter = RangeBufferFilterDings()
+    rospy.init_node("RangeFilter")
+    rangePub = rospy.Publisher('FilteredRangePublisher', Range, queue_size=10)
+    rospy.Subscriber("/turtlebot1/RangePublisher", Range, callback)
